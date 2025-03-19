c29a26a9cb97109cfe8ca7f9b59795a95e20840d
Sunilsangfroid <sunilsangfroid@gmail.com>
2025-03-19 09:24:24 +0530

    Fixed bug: Prevent adding empty tasks

--- todo.py
+++ todo.py
@@ -1,8 +1,11 @@
 # todo.py
 tasks = []
 def add_task(task):
-    tasks.append(task)
-    print(f'Task "{task}" added.')
+    if task.strip() == "":
+        print("Task cannot be empty.")
+    else:
+        tasks.append(task)
+        print(f'Task "{task}" added.')
 def list_tasks():
     if not tasks:
         print("No tasks available.")
@@ -11,4 +14,6 @@ def list_tasks():
             print(f'{idx}. {task}')
 if __name__ == "__main__":
     add_task("Finish Assignment")
+    list_tasks()
+    add_task(" ")
     list_tasks()
